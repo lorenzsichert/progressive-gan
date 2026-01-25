0c81a566fc97c22d20a10297ec962f9cb70d19f1
Lorenz Sichert <lorenz.sichert@proton.me>
2026-01-25 14:59:22 +0100

    back to main

---
+++ models.py
@@ -0,0 +1,203 @@
+import torch.nn as nn
+import torch
+from torch.nn.functional import interpolate
+from torch.nn.utils import spectral_norm
+
+
+
+class InitLayer(nn.Module):
+    def __init__(self, nz, channel):
+        super().__init__()
+
+        self.init = nn.Sequential(
+                        spectral_norm(nn.ConvTranspose2d(nz, channel*2, 4, 1, 0, bias=False)),
+                        nn.BatchNorm2d(channel*2), GLU() )
+
+    def forward(self, noise):
+        noise = noise.view(noise.shape[0], -1, 1, 1)
+        return self.init(noise)
+
+class NoiseInjection(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.weight = nn.Parameter(torch.zeros(1), requires_grad=True)
+
+    def forward(self, feat, noise=None):
+        if noise is None:
+            batch, _, height, width = feat.shape
+            noise = torch.randn(batch, 1, height, width).to(feat.device)
+
+        return feat + self.weight * noise
+
+class GLU(nn.Module):
+    def forward(self, x):
+        nc = x.size(1)
+        assert nc % 2 == 0, 'channels dont divide 2!'
+        nc = int(nc/2)
+        return x[:, :nc] * torch.sigmoid(x[:, nc:])
+
+def UpBlock(in_planes, out_planes):
+    block = nn.Sequential(
+        nn.Upsample(scale_factor=2, mode='nearest'),
+        nn.utils.spectral_norm(nn.Conv2d(in_planes, out_planes*2, 3, 1, 1, bias=False)),
+        NoiseInjection(),
+        nn.BatchNorm2d(out_planes*2), GLU(),
+    )
+    return block
+
+def UpBlockComp(in_planes, out_planes):
+    block = nn.Sequential(
+        nn.Upsample(scale_factor=2, mode='nearest'),
+        spectral_norm(nn.Conv2d(in_planes, out_planes*2, 3, 1, 1, bias=False)),
+        #convTranspose2d(in_planes, out_planes*2, 4, 2, 1, bias=False),
+        NoiseInjection(),
+        nn.BatchNorm2d(out_planes*2), GLU(),
+        spectral_norm(nn.Conv2d(out_planes, out_planes*2, 3, 1, 1, bias=False)),
+        NoiseInjection(),
+        nn.BatchNorm2d(out_planes*2), GLU()
+        )
+    return block
+
+class Generator(nn.Module):
+    def __init__(self, nz, ngf, nc, img_size, layer):
+        super().__init__()
+
+        self.img_size = img_size
+        self.layer = layer
+        self.nc = nc
+
+        nfc_multi = {4:16, 8:8, 16:4, 32:2, 64:2, 128:1, 256:0.5, 512:0.25, 1024:0.125}
+        self.nfc = {}
+        for k, v in nfc_multi.items():
+            self.nfc[k] = int(v*ngf)
+
+        self.init = InitLayer(nz, self.nfc[4])
+        self.features = nn.ModuleList()
+        for i in self.nfc:
+            if i < layer:
+                self.features.append(UpBlockComp(self.nfc[i], self.nfc[i*2]))
+            else:
+                break
+
+        self.to_big_low = nn.Sequential(
+            spectral_norm(nn.Conv2d(self.nfc[layer // 2], self.nc, 3, 1, 1, bias=False))
+        )
+        self.to_big_high = nn.Sequential(
+            spectral_norm(nn.Conv2d(self.nfc[layer], self.nc, 3, 1, 1, bias=False))
+        )
+
+        print(len(self.features))
+
+    def add_layer(self):
+        self.features.append(UpBlockComp(self.nfc[self.layer], self.nfc[self.layer*2]))
+        self.to_big_low = self.to_big_high
+        self.layer *= 2
+        self.to_big_high = nn.Sequential(
+            spectral_norm(nn.Conv2d(self.nfc[self.layer], self.nc, 3, 1, 1, bias=False))
+        )
+
+
+
+    def forward(self, input, alpha=1.0):
+        feature = self.init(input)
+        for i in range(len(self.features)-1):
+            feature = self.features[i](feature)
+
+        big_low = interpolate(self.to_big_low(feature), (self.layer,self.layer))
+        big_high = self.to_big_high(self.features[len(self.features)-1](feature))
+
+        return (1-alpha) * big_low + alpha * big_high
+
+
+
+
+
+
+def downBlockHead(in_planes, out_planes):
+    return nn.Sequential(
+        spectral_norm(nn.Conv2d(in_planes, out_planes, 4, 2, 1, bias=False)),
+        nn.LeakyReLU(0.2, inplace=True)
+    )
+
+def downBlock(in_planes, out_planes):
+    return nn.Sequential(
+        spectral_norm(nn.Conv2d(in_planes, out_planes, 4, 2, 1, bias=False)),
+        nn.BatchNorm2d(out_planes), nn.LeakyReLU(0.2, inplace=True),
+        spectral_norm(nn.Conv2d(out_planes, out_planes, 3, 1, 1, bias=False)),
+        nn.BatchNorm2d(out_planes), nn.LeakyReLU(0.2, inplace=True)
+    )
+
+class DownBlockComp(nn.Module):
+    def __init__(self, in_planes, out_planes):
+        super(DownBlockComp, self).__init__()
+
+        self.main = nn.Sequential(
+            spectral_norm(nn.Conv2d(in_planes, out_planes, 4, 2, 1, bias=False)),
+            nn.BatchNorm2d(out_planes), nn.LeakyReLU(0.2, inplace=True),
+            spectral_norm(nn.Conv2d(out_planes, out_planes, 3, 1, 1, bias=False)),
+            nn.BatchNorm2d(out_planes), nn.LeakyReLU(0.2)
+            )
+
+        self.direct = nn.Sequential(
+            nn.AvgPool2d(2, 2),
+            spectral_norm(nn.Conv2d(in_planes, out_planes, 1, 1, 0, bias=False)),
+            nn.BatchNorm2d(out_planes), nn.LeakyReLU(0.2))
+
+    def forward(self, feat):
+        return (self.main(feat) + self.direct(feat)) / 2
+
+
+class Discriminator(nn.Module):
+    def __init__(self, ndf, nc, img_size, layer):
+        super().__init__()
+        
+        self.img_size = img_size
+        self.layer = layer
+        self.nc = nc
+
+        nfc_multi = {4:16, 8:8, 16:4, 32:2, 64:2, 128:1, 256:0.5, 512:0.25, 1024:0.125}
+        self.nfc = {}
+        for k, v in nfc_multi.items():
+            self.nfc[k] = int(v*ndf)
+
+        self.rf = nn.Sequential(spectral_norm(nn.Conv2d(self.nfc[4], 1, 2, 1, 0)))
+        self.features = nn.ModuleList()
+
+        for i in self.nfc:
+            if i < layer:
+                self.features.append(downBlock(self.nfc[i*2], self.nfc[i]))
+            else:
+                break
+
+
+        self.down_from_big_high = downBlockHead(3, self.nfc[self.layer])
+
+        self.down_from_big_low = downBlockHead(3, self.nfc[self.layer // 2])
+
+
+    def add_layer(self):
+        self.features.append(downBlock(self.nfc[self.layer * 2], self.nfc[self.layer]))
+
+
+        self.down_from_big_low = self.down_from_big_high
+        self.down_from_big_high = downBlockHead(3, self.nfc[self.layer * 2])
+        self.layer *= 2
+
+
+
+    def forward(self, input, alpha):
+        feature_high = self.down_from_big_high(input)
+        feature_high_low = self.features[len(self.features)-1](feature_high)
+
+        input_low = interpolate(input, (self.layer // 2, self.layer // 2))
+        feature_low = self.down_from_big_low(input_low)
+
+
+        feature = (1-alpha) * feature_low + alpha * feature_high_low
+
+        for i in reversed(range(len(self.features)-1)):
+            feature = self.features[i](feature)
+
+        return self.rf(feature)
+
---
+++ train.py
@@ -0,0 +1,187 @@
+from math import log
+from torch import mean, optim
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, dataset
+from torch.utils.data import DataLoader
+from torchvision import transforms
+import torchvision
+from torchvision.utils import save_image
+
+from diffaug import DiffAugment
+
+from models import Generator, interpolate
+from models import Discriminator
+
+# Use as many threads as possible
+torch.set_num_threads(20)
+torch.set_num_interop_threads(20)
+
+n_epochs = 2000
+b1 = 0.5
+b2 = 0.99
+lr_g = 0.0001
+lr_d = 0.0001
+latent_dim = 256
+features = 8
+img_size = 512
+layer = 256
+channels = 3
+batch_size = 16
+discriminator_batch_size = batch_size
+sample_interval = 16
+
+alpha_end = 2.0
+alpha_incease = 0.0001
+alpha_dropdown = 1.0
+counting_alpha = 0.369
+
+load_ckpt = True
+
+
+# --- Dataset Loading ---
+link = "../progressive-gan/iamkaikai/"
+split = "train"
+image_tag = "image"
+
+
+class DatasetTransform(Dataset):
+    def __init__(self, dataset, transform):
+        self.dataset = dataset
+        self.transform = transform
+
+    def __len__(self):
+        return self.dataset.num_rows
+
+    def __getitem__(self, index):
+        item = self.dataset[index][image_tag]
+        if self.transform:
+            item = self.transform(item)
+        return item
+
+def convert_to_rgb(x):
+    return x.convert("RGB")
+
+
+transform = transforms.Compose([
+    transforms.Resize((img_size, img_size)),
+    transforms.ToTensor(),
+    transforms.Normalize([0.5],[0.5])
+])
+
+dataset = torchvision.datasets.ImageFolder(link, transform=transform)
+dataloader = DataLoader(
+    dataset,
+    batch_size=discriminator_batch_size,
+    num_workers=10,
+    shuffle=True,
+    pin_memory=True,
+)
+
+
+
+# --- Cuda Init ---
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+print(f"Running on {device}")
+
+
+
+generator = Generator(nz=latent_dim, ngf=features, img_size=img_size, nc=channels, layer=layer)
+discriminator = Discriminator(ndf=features, nc=channels, img_size=img_size, layer=layer)
+
+if load_ckpt:
+    try:
+        generator.load_state_dict(torch.load(f"ckpt/G-{layer}-a{counting_alpha}.pth"))
+        discriminator.load_state_dict(torch.load(f"ckpt/D-{layer}-a{counting_alpha}.pth"))
+    except:
+        print("Models could not be loaded!")
+
+
+generator.to(device)
+discriminator.to(device)
+
+
+optimizerG = optim.Adam(generator.parameters(), lr=lr_g, betas=(b1, b2))
+optimizerD = optim.Adam(discriminator.parameters(), lr=lr_d, betas=(b1, b2))
+
+
+alpha = 1.0
+
+fixed_noise = torch.randn(batch_size, latent_dim, 1, 1).to(device)
+
+
+
+for ep in range(n_epochs):
+    print(f"Epoch {ep}:")
+
+
+    i = 0
+    for batch in dataloader:
+
+        counting_alpha += alpha_incease
+        if (counting_alpha >= alpha_end and layer < img_size):
+            counting_alpha = 0.0
+            alpha_incease *= alpha_dropdown
+            torch.save(generator.state_dict(), f"G-{layer}.pth")
+            torch.save(discriminator.state_dict(), f"D-{layer}.pth")
+            print("layer")
+            layer *= 2
+            generator.add_layer()
+            discriminator.add_layer()
+
+            generator.to(device)
+            discriminator.to(device)
+
+            optimizerG = optim.Adam(generator.parameters(), lr=lr_g, betas=(b1, b2))
+            optimizerD = optim.Adam(discriminator.parameters(), lr=lr_d, betas=(b1, b2))
+        alpha = min(max(0.0,counting_alpha),1.0)
+        i += 1
+
+
+        # Train Discriminator on Real Images
+        discriminator.zero_grad()
+
+        real = batch[0].to(device)
+        real = interpolate(real, (layer, layer))
+        real = DiffAugment(real, policy="color,translation")
+
+        output_real = discriminator(real, alpha)
+
+        loss_real = mean(nn.functional.relu(1 - output_real))
+        loss_real.backward()
+
+        
+        # Train Discriminator on Fake Images
+        noise = torch.randn(batch_size, latent_dim, 1, 1).to(device)
+        fake = generator(noise, alpha)
+        fake = DiffAugment(fake, policy="color,translation")
+
+        output_fake = discriminator(fake, alpha) 
+
+        loss_fake = mean(nn.functional.relu(1 + output_fake))
+        loss_fake.backward()
+        optimizerD.step()
+ 
+
+        # Train Generator with Discriminator
+        generator.zero_grad()
+        noise = torch.randn(batch_size, latent_dim, 1, 1).to(device)
+        output = generator(noise, alpha)
+        output = DiffAugment(output, policy="color,translation")
+        output_fake = discriminator(output, alpha) 
+        loss_generated = -mean(output_fake)
+        loss_generated.backward()
+        optimizerG.step()
+
+
+
+        if i % (sample_interval/4) == 0:
+            print(f"Ep: {ep}, i: {i}/{len(dataloader)}, alpha: {counting_alpha:.3f}, D(r): {mean(output_real):.3f}, D(f): {mean(output_fake):.3f}, D Loss: {(loss_real + loss_fake)/2:.3f}, G Loss:  {loss_generated:.3f}")
+        if i % sample_interval == 0:
+            save_image(output, f"images/image-{ep}.png", normalize=True)
+            with torch.no_grad():
+                fixed = generator(fixed_noise, alpha)
+                save_image(fixed, f"images/image-f-{ep}.png", normalize=True)
+        if i % (sample_interval * 16) == 0:
+            torch.save(generator.state_dict(), f"ckpt/G-{layer}-a{counting_alpha:.3f}.pth")
+            torch.save(discriminator.state_dict(), f"ckpt/D-{layer}-a{counting_alpha:.3f}.pth")
